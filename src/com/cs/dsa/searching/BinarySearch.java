659514202623a3ce1729611351f7aa1b0791cbd6
badripaudel77 <badripaudel77@gmail.com>
2020-08-18 23:00:20 +0545

    Searching algorithms added - Binary and  Linear Searching

---
+++ src/com/cs/dsa/searching/BinarySearch.java
@@ -0,0 +1,69 @@
+package com.cs.dsa.searching;
+
+/*
+  Binary Search is done on the sorting array. Its complexity is O(log n).
+  Follows " Divide and conquer " approach.
+
+  steps :
+        1) Find the middle element, if that is the target element, just return that element, middleIndex = lower + (upper - lower)/2
+        2) If target element is > than middle then see the right part of the array [list] else left part and do step 1
+        3) If match is found just return index or element else return -1
+*/
+
+import java.util.Arrays;
+
+public class BinarySearch extends  Object{
+
+    public BinarySearch() {}
+
+    public static void main(String[] args) {
+
+        int elements[] = { 12, 34, 45,77 };
+        int elementToSearch = 77;
+
+        System.out.print("Original Array is "+ Arrays.toString(elements));
+        searchElement(elements, elementToSearch);
+    }
+
+    //binary search
+    public  static void searchElement(int [] elements, int elementToSearch) {
+
+        //elements is a sorted array as it has to be sorted.
+        int lenOfElements = elements.length;
+        int i;
+        int noOfItrToFindElem = 0;
+        Integer index = null;
+        Integer middleIndex = null;
+        int stepsToFindElem = 0;
+
+        int firstIndex = 0;
+        int lastIndex = lenOfElements -1;
+
+        if(lenOfElements <= 0) {
+            System.out.println("\nPlease provide some elements");
+            return;
+        }
+
+        while (lastIndex >= firstIndex) {
+
+            stepsToFindElem ++;
+
+            middleIndex = firstIndex + (lastIndex- firstIndex) /2;
+
+            if(elements[middleIndex] == elementToSearch) {
+                index = middleIndex;
+                System.out.println("\nElement found at  index "+ index + " at " + stepsToFindElem + " steps.");
+                return;
+            }
+            if(elements[middleIndex] > elementToSearch) {
+                //element is in left half so decrease the upper bound
+                lastIndex = lastIndex -1;
+            }
+
+            if(elements[middleIndex] < elementToSearch) {
+               //element is in the right half so increase the lower bound
+                firstIndex = firstIndex + 1;
+            }
+        }
+    }
+}
---
+++ src/com/cs/dsa/searching/LinearSearch.java
@@ -0,0 +1,144 @@
+package com.cs.dsa.searching;
+
+/*
+
+   We do linear search of any items.
+   Linear simply mean fron left to right [ r ti l ]  as in daily life.
+   Array Search , if not found returns -1.
+
+  - has time complexity of n i.e Big Oh of n O(n) as time is linearly dependent on input size of an array
+
+ */
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class LinearSearch {
+
+    public static void main(String[] args) {
+
+        int itmes[] = {12, 34, 5, 6, 67};
+        int itemToSearch = 6;
+
+        int itemToUpdate = 6;
+        int updateValue = 23;
+
+        int itemToDelete = 12;
+
+        //call the method [for searching]
+        System.out.println("original array is " + Arrays.toString(itmes));
+        LinearSearch.searchElement(itmes, itemToSearch);
+        System.out.println("\n");
+
+        //update the element
+        System.out.println("original array is " + Arrays.toString(itmes));
+        LinearSearch.updateElement(itmes, itemToUpdate, updateValue);
+        System.out.println("\n");
+
+        //delete an element
+        System.out.println("original array is " + Arrays.toString(itmes));
+        LinearSearch.deleteElement(itmes, itemToDelete);
+        System.out.println("\n");
+
+    }
+
+    //method for linear search
+    public static void searchElement(int[] items, int itemToFind) {
+        //length of an array items
+        int lenOfItems = items.length;
+        int i;
+        int iteratinsToFindElem = 0;
+        boolean flag = false;
+
+        if (lenOfItems <= 0) {
+            System.out.println("please pass some items.");
+            return;
+        }
+
+        //traverse each array items , for n inputs it has to iterate n times, so complexity is O(n)
+        for (i = 0; i < lenOfItems; i++) {
+            iteratinsToFindElem++;
+
+            if (items[i] == itemToFind) {
+                flag = true;
+                break;
+            }
+        }
+        if (flag)
+            System.out.println("Yes the element " + itemToFind + " is present in the given items array. And it is found after "+ iteratinsToFindElem + " iterations.");
+        else
+            System.out.println("Sorry the element " + itemToFind + " is not present in the given items array.");
+    }
+
+    //update element
+    public static void updateElement(int[] items, int elemToUpdate, int updateValue) {
+        //length of an array items
+        int lenOfItems = items.length;
+        int i;
+        Integer index = null;
+
+        if (lenOfItems <= 0) {
+            System.out.println("Please pass some elements.");
+            return;
+        }
+
+        //traverse each array items , for n inputs it has to iterate n times, so complexity is O(n)
+        for (i = 0; i < lenOfItems; i++) {
+            if (items[i] == elemToUpdate) {
+                //if item is found, then set index to i
+                index = i;
+                break;
+            }
+        }
+
+        //only update if index is non-null
+        if (index == null) {
+            System.out.println("sorry the element "+ elemToUpdate + " is not present in items array.");
+            return;
+        }
+        else items[i] = updateValue;
+
+        System.out.print("After updating an element " + elemToUpdate + " at index " + index + "array is : ");
+        for (i = 0; i < lenOfItems; i++) {
+
+            System.out.print(" " + items[i]);
+        }
+    }
+
+    //delete the item from items
+    public static void deleteElement(int items[], int itemToDelete) {
+        //length of an array items
+        int lenOfItems = items.length;
+        int i;
+        Integer index = null;
+
+        if (lenOfItems <= 0) {
+            System.out.println("Please pass some elements. ");
+            return;
+        }
+
+        //traverse each array items ,for n inputs it has to iterate n times, so complexity is O(n)
+        for (i = 0; i < lenOfItems; i++) {
+            if (items[i] == itemToDelete) {
+                //if item is found, then set index to i
+                index = i;
+                break;
+            }
+        }
+            if (index == null) {
+                System.out.println("sorry the element " + itemToDelete + " is not present.");
+                return;
+            }
+
+            int newLen = lenOfItems -1;
+            for (i = 0; i < newLen; i++) {
+                items[index] = items[index + 1];
+            }
+
+            System.out.print("After deleting an item " + itemToDelete +" at index " + index + " the array is : ");
+            for (i = 0; i < newLen; i++) {
+                System.out.print(" " + items[i]);
+            }
+        }
+    }
+
